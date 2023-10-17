aff519300b608ffdc869c0799005b375633e4004
snehcb <sneh@crowdbotics.com>
2023-10-17 17:05:50 +0530

    updated login code

--- cypress/integration/api/tests/logintest.js
+++ cypress/integration/api/tests/logintest.js
@@ -2,29 +2,6 @@
 /// <reference types = "cypress"/>
 import { doLogin } from 'D:/crowdbotics_automation/cypress/integration/api/pages/loginPage.js';
 
-<<<<<<< HEAD
-
-describe("Checkout flow", () => {
-    let auth_key;
-    const login = new LoginPage();
-
-    it('POST request', () => {
-         let response = login.login();
-
-    it('POST request', function () {
-        login.doLogin();
-        // login.doLogin().then((res) =>{
-        //     cy.log('json data    ===========', res)
-        // })
-
-
-        // expect(response.status).to.eq(200)
-        // auth_key = response.body.key
-        // cy.log('api auth key is generated ', auth_key)
-    })
-})
-});
-=======
 describe("Login flow", () => {
     it('login request', () => {
         doLogin().then((response) => {
@@ -35,4 +12,3 @@ describe("Login flow", () => {
 })
 
 
->>>>>>> 972398ae658735eb35aa9cedd78bd2e2ba58e90f
