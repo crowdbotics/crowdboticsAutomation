3e378ddc8857e53a836cdf1a5d795c0402bb742d
Himanshu1231231 <himanshu.sharma@crowdbotics.com>
2023-10-30 15:53:48 +0530

    Automated 5 test cases of PRD & catalog

--- cypress/integration/api/pages/PrdPage.js
+++ cypress/integration/api/pages/PrdPage.js
@@ -2,11 +2,11 @@
 
 export const prd_overview_tags = (auth_key, app_id) => {
 
-    cy.readFile('cypress/fixtures/createTags.json').then((data) => {
+    cy.readFile('cypress/fixtures/api_createTags.json').then((data) => {
         data.app = app_id
-        cy.writeFile('cypress/fixtures/createTags.json', JSON.stringify(data))
+        cy.writeFile('cypress/fixtures/api_createTags.json', JSON.stringify(data))
     })
-    return cy.fixture('createTags.json').then((myFixture) => {
+    return cy.fixture('api_createTags.json').then((myFixture) => {
         cy.request({
             method: 'POST',
             url: Cypress.env('baseUrl') + Cypress.env('createTags')+app_id,
@@ -30,19 +30,16 @@ export const prd_overview_tags = (auth_key, app_id) => {
 
 export const prd_overview_userRoles = (auth_key, app_id) => {
 
-    cy.readFile('cypress/fixtures/createUserRoles.json').then((data) => {
+    cy.readFile('cypress/fixtures/api_createUserRoles.json').then((data) => {
         data.app = app_id
-        cy.writeFile('cypress/fixtures/createUserRoles.json', JSON.stringify(data))
+        cy.writeFile('cypress/fixtures/api_createUserRoles.json', JSON.stringify(data))
     })
-    return cy.fixture('createUserRoles.json').then((myFixture) => {
+    return cy.fixture('api_createUserRoles.json').then((myFixture) => {
         cy.request({
             method: 'POST',
             url: Cypress.env('baseUrl') + Cypress.env('createUserRoles')+app_id,
             body: myFixture,
             headers: {
-
-                
-
 
                 // 'Content-Type': 'application/json',
                 // 'Accept': 'application/json',
@@ -55,6 +52,74 @@ export const prd_overview_userRoles = (auth_key, app_id) => {
     })
 };
 
+export const prd_AI = (auth_key, app_id,app_name1) => {
 
+    cy.readFile('cypress/fixtures/api_createprdAi.json').then((data) => {
+        data.app_name = app_name1
+        cy.writeFile('cypress/fixtures/api_createprdAi.json', JSON.stringify(data))
+    })
+    return cy.fixture('api_createprdAi.json').then((myFixture) => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('baseUrl') + Cypress.env('createprdai1')+app_id+Cypress.env('createprdai2'),
+            body: myFixture,
+            headers: {
 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Token ' + auth_key,
+            }
+           
+        }).then((response) => {
+            return response;            
+        })
+    })
+};
 
+export const prd_catalog_templates = (auth_key, app_id) => {
+
+    cy.readFile('cypress/fixtures/api_catalogTemplates.json').then((data) => {
+        data.app = app_id
+        cy.writeFile('cypress/fixtures/api_catalogTemplates.json', JSON.stringify(data))
+    })
+    return cy.fixture('api_catalogTemplates.json').then((myFixture) => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('baseUrl') + Cypress.env('createcatalogtemplates')+app_id,
+            body: myFixture,
+            headers: {
+
+                // 'Content-Type': 'application/json',
+                // 'Accept': 'application/json',
+                'Authorization': 'Token ' + auth_key,
+            }
+           
+        }).then((response) => {
+            return response;            
+        })
+    })
+};
+
+export const prd_add_features = (auth_key, app_id) => {
+
+    cy.readFile('cypress/fixtures/api_createprdFeature.json').then((data) => {
+        data.app = app_id
+        cy.writeFile('cypress/fixtures/api_createprdFeature.json', JSON.stringify(data))
+    })
+    return cy.fixture('api_createprdFeature.json').then((myFixture) => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('baseUrl') + Cypress.env('createprdfeature')+app_id,
+            body: myFixture,
+            headers: {
+
+                // 'Content-Type': 'application/json',
+                // 'Accept': 'application/json',
+                'Authorization': 'Token ' + auth_key,
+            }
+           
+        }).then((response) => {
+            return response;            
+        })
+    })
+};
--- cypress/integration/api/tests/PrdPageTest.js
+++ cypress/integration/api/tests/PrdPageTest.js
@@ -1,10 +1,11 @@
 /// <reference types = "cypress"/>
 import {doCteareApp} from '../pages/DashboardPage.js';
 import {doLogin} from '../pages/loginPage.js';
-import {prd_overview_tags,prd_overview_userRoles} from '../pages/PrdPage.js';
+import {prd_overview_tags,prd_overview_userRoles,prd_AI,prd_catalog_templates,prd_add_features} from '../pages/PrdPage.js';
 
 let authKey;
 let app_id;
+let app_name;
 describe("PRD Page", () => {
     let app_name = 'TestAPIAutoSettings' + (Math.random() + 1).toString(36).substring(7);
     it('Create Tags Flow', () => {
@@ -14,6 +15,7 @@ describe("PRD Page", () => {
                 cy.log("login response", response.body)
                 expect(response.body.name, "App name is not matching").to.eq(app_name)
                 app_id = response.body.id;
+                app_name = response.body.name;
                 localStorage.setItem('app_id', response.body.id);
                 // const app_id = localStorage.getItem('app_id');
                 prd_overview_tags(authKey, app_id).then((response) =>{
@@ -23,10 +25,36 @@ describe("PRD Page", () => {
             })
         })
     })
+    
     it('Create UserRoles Flow', () => {
                 prd_overview_userRoles(authKey, app_id).then((response) =>{
                     expect(response.status).to.eq(201)
-                    cy.log("create tags response", response.body)
+                    cy.log("create userroles response", response.body)
                 })
             })
-        })  
+         
+
+    it('Create PRDAI Flow', () => {
+            prd_AI(authKey, app_id, app_name).then((response) =>{
+                expect(response.status).to.eq(202)
+                cy.log("create AI response", response.body)
+            })
+        })
+
+    it('Create PRDCatalogTemplates Flow', () => {
+        prd_catalog_templates(authKey, app_id).then((response) =>{
+                expect(response.status).to.eq(200)
+                cy.log("create catalogtemplates response", response.body)
+            })
+        })
+
+    it('Create PRDFeature Flow', () => {
+        prd_add_features(authKey, app_id).then((response) =>{
+                    expect(response.status).to.eq(200)
+                    cy.log("create PRDFeature response", response.body)
+                })
+            })    
+        
+    })
+    
+
