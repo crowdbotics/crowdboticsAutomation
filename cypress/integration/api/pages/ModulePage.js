45f876137f6e8b28f863e7886c3fc647f7320f1b
Mithilesh <mithilesh@crowdbotics.com>
2024-04-04 17:17:46 +0530

    Added 3 New Test cases

--- cypress/integration/api/pages/ModulePage.js
+++ cypress/integration/api/pages/ModulePage.js
@@ -28,4 +28,34 @@ export const moduleInstall = (auth_key, app_id) => {
     }).then((response) => {
         return response;
     })
+    }
+
+export const doGetmodulelist = (auth_key,app_id,module_id) => {
+
+    return cy.request({
+     method: 'GET',
+     url: Cypress.env('baseUrl') + Cypress.env('getmoduleslist1') + app_id + Cypress.env('getmoduleslist2')+module_id,
+     headers: {
+         // 'Content-Type': 'application/json',
+         // 'Accept': 'application/json',
+         'Authorization': 'Token ' + auth_key,
+     }
+ }).then((response) => {
+     return response;
+ })
 }
+export const doCreateModules = (auth_key, app_id) => {
+    return cy.fixture('api_post_Modules_Create.json').then((myFixture) => {
+        cy.request({
+            method: 'POST',
+            url: Cypress.env('baseUrl') + Cypress.env('postCreatemodules1') + app_id + Cypress.env('postCreatemodules2'),
+            body: myFixture,
+            headers: {
+                // 'Content-Type': 'application/json',
+                // 'Accept': 'application/json',
+                'Authorization': 'Token ' + auth_key,
+            }
+        }).then((response) => {
+            return response;
+        })
+    })}
