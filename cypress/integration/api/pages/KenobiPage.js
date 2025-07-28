ba94425eedcdb9cfaf13b1c63234d6f0a2693917
anishcb <anish.kumar@crowdbotics.com>
2025-07-28 13:40:42 +0530

    Kenobi with zip file

--- cypress/integration/api/pages/KenobiPage.js
+++ cypress/integration/api/pages/KenobiPage.js
@@ -223,6 +223,134 @@ export const doDeleteKenobiC2SUsingPublicRepoById = (auth_key, kenobi_id) => {
     })
 };
 
+export const doCreateKenobiC2SUsingZipFile = (auth_key) => {
+     if (!auth_key) {
+        throw new Error("auth_key is required");
+    }
+  return cy.fixture('TestingRepo-main.zip', 'base64').then((zipBase64) => {
+    return cy.fixture('api_Create_Kenobi_C2S_Zip.json').then((myFixture) => {
+      myFixture.file = zipBase64;
+      return cy.request({
+        method: 'POST',
+        url: Cypress.env('baseUrl') + Cypress.env('CreateKenobiC2S'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Token ' + auth_key,
+        },
+        body: myFixture,
+      }).then((response) => {
+        return response;
+      });
+
+    });
+
+  });
+};
+
+export const doGetKenobiC2SUsingZipFileById = (auth_key, kenobi_id) => {
+    if (!auth_key) {
+        throw new Error("auth_key is required");
+    }
+
+    if (!kenobi_id) {
+        throw new Error("kenobi_id is required");
+    }
+    return cy.request({
+        method: 'GET',
+        url: Cypress.env('baseUrl') + Cypress.env('CreateKenobiC2S') + kenobi_id + Cypress.env('CreateKenobiC2S1'),
+        headers: {
+            'Authorization': 'Token ' + auth_key
+        }
+    }).then((response) => {
+        return response;
+    })
+};
+export const doPutKenobiC2SUsingZipFile = (auth_key, kenobi_id) => {
+    if (!auth_key) {
+        throw new Error("auth_key is required");
+    }
+
+    if (!kenobi_id) {
+        throw new Error("kenobi_id is required");
+    }
+  return cy.fixture('knobitestpublic-main.zip', 'base64').then((zipBase64) => {
+    return cy.fixture('api_Put_Kenobi_C2S_Zip_File.json').then((myFixture) => {
+      myFixture.file = zipBase64;
+      return cy.request({
+        method: 'POST',
+        url: Cypress.env('baseUrl') + Cypress.env('CreateKenobiC2S') + kenobi_id + Cypress.env('CreateKenobiC2S1'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Token ' + auth_key,
+        },
+        body: myFixture,
+      }).then((response) => {
+        return response;
+      });
+
+    });
+
+  });
+};
+export const doPatchKenobiC2SUsingZipFile = (auth_key, kenobi_id) => {
+    if (!auth_key) {
+        throw new Error("auth_key is required");
+    }
+
+    if (!kenobi_id) {
+        throw new Error("kenobi_id is required");
+    }
+  return cy.fixture('TestingRepo-main.zip', 'base64').then((zipBase64) => {
+    return cy.fixture('api_Patch_Kenobi_C2S_Zip_File.json').then((myFixture) => {
+      myFixture.file = zipBase64;
+      return cy.request({
+        method: 'POST',
+        url: Cypress.env('baseUrl') + Cypress.env('CreateKenobiC2S') + kenobi_id + Cypress.env('CreateKenobiC2S1'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Token ' + auth_key,
+        },
+        body: myFixture,
+      }).then((response) => {
+        return response;
+      });
+
+    });
+
+  });
+};
+
+export const doDeleteKenobiC2SUsingZipFileById = (auth_key, kenobi_id) => {
+    if (!auth_key) {
+        throw new Error("auth_key is required");
+    }
+
+    if (!kenobi_id) {
+        throw new Error("kenobi_id is required");
+    }
+    return cy.request({
+        method: 'DELETE',
+        url: Cypress.env('baseUrl') + Cypress.env('CreateKenobiC2S') + kenobi_id + Cypress.env('CreateKenobiC2S1'),
+        headers: {
+            'Authorization': 'Token ' + auth_key
+        }
+    }).then((response) => {
+        return response;
+    })
+};
+
+
+
+
+//doPutKenobiC2SUsingZipFile,doPatchKenobiC2SUsingZipFile,doDeleteKenobiC2SUsingZipFileById
+
+
+
+
+
 
 
 
